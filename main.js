272f4cff59eaa40de6dcb62d729773b816116468
ailen alvarez <ailennalvarez4@gmail.com>
2021-11-03 23:10:44 -0300

    Desafio DOM

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ const productoAgregado=()=>{
 
 productosSamgung.push(new Productos("Galaxy S10",1000,"blanco"));
 productosSamgung.push(new Productos("Galaxy S20",2000,"amarillo"));
+productosSamgung.push(new Productos("Galaxy S30",3000,"rojo"));
 productosIphone.push(new Productos("Iphone X",3000,"rojo"));
 productosIphone.push(new Productos("Iphone XS",4000,"negro"));
 
@@ -46,3 +47,22 @@ let mercaderiaFinal=productosSamgung.concat(productosIphone);
 for (obj of mercaderiaFinal){
     console.log(obj.modelo);
 }
+
+//dom
+
+let nuevosProductos= document.createElement("p");
+nuevosProductos.setAttribute("id","nuevosProductos");
+nuevosProductos.innerHTML= "En esta seccion les estaremos contando las novedades de la tienda, descuentos exclusivos, y proximas llegadas";
+document.getElementById("novedades").appendChild(nuevosProductos);
+console.log(nuevosProductos);
+
+// for of con dom
+
+for(const objetos of productosSamgung){
+    let li= document.createElement("li");
+    li.innerHTML=objetos.modelo;
+    document.getElementById("productosSamsung").appendChild(li);
+    li.style.textAlign="center";
+    li.style.margin="20px";
+}
+
